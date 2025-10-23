b9c3d6c9897240a9f6a764d175bbd9fa17e8b704
sr_wandersson <161793765+wanderssong@users.noreply.github.com>
2025-10-23 16:57:18 -0400

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+const ultimaAtualizacao = "23/10/2025"; // SEMPRE ATUALIZAR
+const dadosEmpresas = [
+    {
+        nome: "ESPAÇO FIT DA MARI",
+        depositos: [
+            { data: "16-10-2025", valor: 500 },
+            
+            
+            
+        ],
+        gastos: [
+            { data: "16-10-2025", valor: 50.00, descricao: "Hyper Drynk" },
+            { data: "21-10-2025", valor: 50.00, descricao: "sunset" },
+           
+
+        ]
+    },
+    
+    
+];
