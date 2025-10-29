54afff7dbe8124027e43344dd50e8bc1fc25b473
sr_wandersson <161793765+wanderssong@users.noreply.github.com>
2025-10-29 15:00:25 -0400

    Update last update date and add new expense
    Updated the last update date and added a new expense entry.

--- dados.js
+++ dados.js
@@ -1,4 +1,4 @@
-const ultimaAtualizacao = "24/10/2025"; // SEMPRE ATUALIZAR
+const ultimaAtualizacao = "29/10/2025"; // SEMPRE ATUALIZAR
 const dadosEmpresas = [
     {
         nome: "ESPAÇO FIT DA MARI",
@@ -12,6 +12,7 @@ const dadosEmpresas = [
             { data: "16-10-2025", valor: 50.00, descricao: "Hyper Drynk" },
             { data: "21-10-2025", valor: 50.00, descricao: "sunset" },
             { data: "24-10-2025", valor: 70.00, descricao: "Shake" },
+            { data: "29-10-2025", valor: 80.00, descricao: "Hyper Drink" },
 
         ]
     },
