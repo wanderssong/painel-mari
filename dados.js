cc2baed50647c63972b822930597b19c75d69f77
sr_wandersson <161793765+wanderssong@users.noreply.github.com>
2025-11-14 11:34:09 -0400

    Update last update date and add new deposit entry
    Updated the last update date and added a new deposit entry for 14-11-2025 with a value of 550.

--- dados.js
+++ dados.js
@@ -1,9 +1,10 @@
-const ultimaAtualizacao = "12/11/2025"; // SEMPRE ATUALIZAR
+const ultimaAtualizacao = "14/11/2025"; // SEMPRE ATUALIZAR
 const dadosEmpresas = [
     {
         nome: "ESPAÇO FIT DA MARI",
         depositos: [
             { data: "16-10-2025", valor: 500 },
+            { data: "14-11-2025", valor: 550 },
             
             
             
@@ -17,6 +18,7 @@ const dadosEmpresas = [
             { data: "06-11-2025", valor: 80.00, descricao: "video Hyper" },
             { data: "11-11-2025", valor: 80.00, descricao: "perfil Hyper" },
             { data: "12-11-2025", valor: 40.00, descricao: "SECA BARRIGA" },
+            { data: "14-11-2025", valor: 80.00, descricao: "DETOX" },
 
         ]
     },
