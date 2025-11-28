11b6917749d81847d16c9754d858e2313bbd601c
sr_wandersson <161793765+wanderssong@users.noreply.github.com>
2025-11-28 16:36:45 -0400

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,4 +1,4 @@
-const ultimaAtualizacao = "14/11/2025"; // SEMPRE ATUALIZAR
+const ultimaAtualizacao = "28/11/2025"; // SEMPRE ATUALIZAR
 const dadosEmpresas = [
     {
         nome: "ESPAÇO FIT DA MARI",
@@ -21,6 +21,7 @@ const dadosEmpresas = [
             { data: "14-11-2025", valor: 80.00, descricao: "DETOX" },
             { data: "18-11-2025", valor: 80.00, descricao: "clima natalino" },
             { data: "21-11-2025", valor: 80.00, descricao: "Video Hyper" },
+            { data: "28-11-2025", valor: 80.00, descricao: "Espaço fit" },
 
         ]
     },
